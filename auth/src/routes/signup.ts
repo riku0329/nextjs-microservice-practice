f86ffcfde758fae8d42458c0a0920db7abeaec08
riku0329 user name <riku03290@gmail.com>
2020-08-04 12:33:52 +0900

    Adding Password Hashing

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-vlidation-error';
 import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -20,11 +22,16 @@ router.post(
       throw new RequestValidationError(errors.array());
     }
     const { email, password } = req.body;
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
 
-    console.log('Creating a user');
+    if (existingUser) {
+      throw new BadRequestError('Email in use');
+    }
 
-    res.send({});
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
