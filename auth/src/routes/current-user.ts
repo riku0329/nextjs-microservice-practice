096bf54a97ba6768eff3385c5125ce38be68c475
riku0329 user name <riku03290@gmail.com>
2020-08-11 21:29:12 +0900

    create npm package

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -5,7 +5,7 @@ import { requireAuth } from '../middlewares/request-auth';
 const router = express.Router();
 
 router.get('/api/users/currentuser', currentUser, requireAuth, (req, res) => {
-  res.send({ currentuser: req.currentUser || null });
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
