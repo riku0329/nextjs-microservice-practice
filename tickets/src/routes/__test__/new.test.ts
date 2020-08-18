e600897c9fbcbbbc8d019f92c0368b14ad9c0356
riku0329 user name <riku03290@gmail.com>
2020-08-18 23:06:24 +0900

    Create Tickets test and ticket model

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -7,12 +7,60 @@ it('has a route handler listening to /api/tickets for post requests', async () =
 });
 
 it('can only be accessed if the user is signed in', async () => {
-  const response = await request(app).post('/api/tickets').send({})
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({});
   expect(response.status).not.toEqual(401);
 });
 
-it('returns an error if an invalid title is provided', async () => {});
+it('returns an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: '',
+      price: 10,
+    })
+    .expect(400);
 
-it('returns an error if an invalid price is provided', async () => {});
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      price: 10,
+    })
+    .expect(400);
+});
 
-it('creates a tickets with valid inputs', async () => {});
+it('returns an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'test',
+      price: -10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'test',
+    })
+    .expect(400);
+});
+
+it('creates a tickets with valid inputs', async () => {
+  // チケットをdbの保存する
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'test',
+      price: 20,
+    })
+    .expect(201);
+
+});
