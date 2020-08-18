d652a0e832e7971ef1ec740a666ebf4e4923eca2
riku0329 user name <riku03290@gmail.com>
2020-08-18 20:23:25 +0900

    create test

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,8 +2,8 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@rkltickets/common';
-
+import { errorHandler, NotFoundError, currentUser } from '@rkltickets/common';
+import { createTicketsRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,6 +14,10 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+
+app.use(currentUser)
+
+app.use(createTicketsRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
